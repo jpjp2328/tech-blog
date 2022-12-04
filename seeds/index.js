afeb6a79a2fae586e2a9cd2f4abd62bd535dd723
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-05 01:11:51 +1100

    seedAll added

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const seedUsers = require('./userData');
+const seedComments = require('./comment-seeds');
+const seedPosts = require('./post-seeds')
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  await seedComments();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  await seedPosts();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
