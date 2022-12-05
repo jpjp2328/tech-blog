55159c9c7689780215d0853ccffe4a695ca16fc8
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-05 22:50:32 +1100

    homeroute, get by id added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,38 @@ router.get('/', (req, res) => {
 });
 
 // Get post by ID
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id','heading','content', 'created_at'],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'text', 'user_id', 'post_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }]
+    })
+    .then((postData) => {
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id"});
+            return;
+        }
+        const post = postData.get({ plain: true });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // Login route
 
