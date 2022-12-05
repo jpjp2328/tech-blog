76c9e8c493bdacf17cd95f9d888ec3b1301970ad
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-05 18:48:25 +1100

    get by id route added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -24,6 +24,33 @@ router.get('/', (req, res) => {
 });
 
 // GET by ID
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id','heading','content'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'text', 'user_id', 'post_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }]
+    })
+    .then((postData) => {
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id"});
+            return;
+        }
+        res.json(postData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // POST Creating new post
 
