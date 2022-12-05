a4ca5a27200f017b12563120363131ac3e34c381
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-05 21:47:09 +1100

    bug fix for testing, seeding order matters

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -28,6 +28,8 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers })
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
