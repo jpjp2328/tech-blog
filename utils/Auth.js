f304dba423f03163e19513258c9e07e9db55a407
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-06 05:57:50 +1100

    bug fix with Auth syntax, dashboard now showing

--- utils/Auth.js
+++ utils/Auth.js
@@ -1,6 +1,6 @@
 //Redirects the user if not Logged in
 const withAuth = (req, res, next) => {
-    if(!req.session.logged_in) {
+    if(!req.session.loggedIn) {
         res.redirect('/login');
     } else {
         next();
